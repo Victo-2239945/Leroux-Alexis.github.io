8889bf03376e509965299666dfd4dd86d68323ea
Leroux-Alexis <2239945@carrefour.cegepvicto.ca>
2023-12-07 15:27:43 -0500

    Commit final

--- scripts.js
+++ scripts.js
@@ -30,9 +30,36 @@ function checkForMatch() {
   isMatch ? disableCards() : unflipCards();
 }
 
+//variable utile a compter les cartes désactiver()
+let compterCarteDisable = 0;
+
+
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  //Lors de chaque pairs désactivé, cela les comptes
+  compterCarteDisable += 2;
+  
+  //Fait disparaître les cartes lorsqu'elles sont un match
+  firstCard.classList.add('hidden');
+  secondCard.classList.add('hidden');
+
+  //Si le nombre de cartes désactivé est équivalent au nombres de cartes, le temps s'arrete.
+  if (compterCarteDisable === cards.length) {
+    ArreterTemps();
+    if(compteur<=20){
+      afficherMessage("WOW TEMPS EXCEPTIONNEL :" + compteur + " secondes.");
+    }
+    else if(compteur<=35){
+      afficherMessage("Bien Joué tu es dans la moyenne : " + compteur + " secondes.");
+    }
+    else{
+      afficherMessage("Ce n'était vraiment pas ta meilleure : " + compteur + " secondes.");
+    }
+    
+
+  }
 
   resetBoard();
 }
@@ -61,3 +88,61 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+let compteur = 0;
+let timeout;
+let timer_on = 0;
+
+//fonction qui sert a afficher ainsi que compter le temps.
+function CompterTemps() {
+  document.getElementById("timerCompter").value = compteur;
+  compteur++;
+  timeout = setTimeout(CompterTemps,1000);
+}
+//Function qui arrete le temps.
+function ArreterTemps(){
+  clearTimeout(timeout); 
+}
+
+  let boutonStart=document.getElementById("start");
+   
+  let inputTimerCompter
+
+  boutonStart=addEventListener('click',CommencerCompte);
+  
+  
+  
+//Fonction qui commence le temps losrsqu'on clique sur le bouton commencer le temps ou partout dans l'écran.
+function CommencerCompte() {
+  if (!timer_on) {
+    timer_on = 1;
+    CompterTemps();
+  }
+}
+//Fonction qui sert a afficher des message.
+function afficherMessage(message) {
+  // Afficher le message où vous le souhaitez, par exemple dans une boîte d'alerte
+  alert(message);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
